e8f068b5306503707dc97a58616e0df9658a3832
chriskimty <christaeyoung.kim@gmail.com>
2023-02-06 21:55:06 -0500

    added firebase logic

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
-<script type = "module">
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js"; 
-</script>
+import  firebaseApp  from './firebase.js';
+import {
+  get,
+  ref,
+  getDatabase,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBXP6Y9THtiIK1qfh_pkqyP68o-Gb0isPc",
-  authDomain: "drag-race-9a259.firebaseapp.com",
-  projectId: "drag-race-9a259",
-  storageBucket: "drag-race-9a259.appspot.com",
-  messagingSenderId: "786889324051",
-  appId: "1:786889324051:web:67da2a403c36be7db971e6"
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-const database = getDatabase(firebaseApp)
+const database = getDatabase(firebaseApp);
 const dbRef = ref(database);
-const queenData = get(dbRef).then((snapshot) => {
-    // One of the returned values is a method called ".exists()", which will return a boolean value for whether there is a returned value from our "get" function 
-    if(snapshot.exists()){
-      // We call `.val()` on our snapshot to get the contents of our data. The returned data will be an object that we can  iterate through later
-      console.log(snapshot.val())
-    }else{
-      console.log("No data available")
+var queenArray = [];
+
+const logArrayData = () => {
+  console.log(queenArray[0].image_url)
+}
+
+get(dbRef)
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      queenArray = snapshot.val();
+      logArrayData();
+    } else {
+      console.log("No data available");
     }
-  }).catch((error) => {
-    console.log(error)
   })
+  .catch((error) => {
+    console.log(error);
+  });
 
-  console.log(queenData)
 // Create an app object
-// import firebaseApp from "./firebase";
-
   const app = {};
 
 // DOM Elements
@@ -154,4 +148,4 @@ app.init = function () {
 };
 
 // call the init function
-app.init();
+// app.init();
