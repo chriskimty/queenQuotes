3b815d61068850a152ef9633e5b5c87e8e9a1faf
chriskimty <christaeyoung.kim@gmail.com>
2023-02-19 16:33:27 -0500

    added answer options

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import  firebaseApp  from './firebase.js';
+import firebaseApp from "./firebase.js";
 import {
   get,
   ref,
@@ -23,52 +23,62 @@ app.quizSection = document.querySelector("#quizSection");
 app.startGame = document.querySelector(".startGame");
 app.queenArray = [];
 
-
-app.firebaseCall = () => {get(dbRef)
-  .then((snapshot) => {
-    if (snapshot.exists()) {
-      app.queenArray = snapshot.val().queens;
-      app.randomizer(app.queenArray);
-    } else {
-      console.log("No data available");
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
-
 // randomizer function to get a random queen index from the array
 app.randomizer = (queenArray) => {
   const randomIndex = Math.floor(Math.random() * app.queenArray.length);
-  // return queenArray.queens[randomIndex];
-  console.log(app.queenArray[randomIndex])
+  // console.log(app.queenArray[randomIndex])
+  return app.queenArray[randomIndex];
+};
+
+app.firebaseCall = () => {
+  get(dbRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+      app.queenArray = snapshot.val().queens;
+      app.queen1 = app.randomizer(app.queenArray);
+      app.queen2 = app.randomizer(app.queenArray);
+      app.displayQuote();
+      app.displayAnswers();
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+};
+
+app.displayQuote = () => {
+  app.quoteContainer.innerHTML = app.queen1.quote;
+};
+
+app.displayAnswers = () => {
+  app.possibleAnswers = [app.queen1.name, app.queen2.name];
+  console.log(app.possibleAnswers);
+     app.possibleAnswers.forEach((element) => {
+      const listElement = document.createElement("li");
+      const queenOptions = document.createTextNode(element)
+      listElement.appendChild(queenOptions)
+      listElement.classList.add("button", "answers");
+      app.ulContainer.appendChild(listElement)
+    })
 };
 
 // NEXT STEPS
-// Use app.randomizer to select the queen1
-// Display queen1 quote
-// Display queen1 name(button)
 // Run app.randomizer to select queen2 Name
-
-
-// Function to select a queen
-app.selectQueen = () => {
-
-}
 
 // Function to run events
 app.events = function () {
   app.hiddenButton.addEventListener("click", function (e) {
-    e.preventDefault()
-    window.location.reload()
-    window.location.assign('index.html#quizSection')
+    e.preventDefault();
+    window.location.reload();
+    window.location.assign("index.html#quizSection");
   });
 
   app.startGame.addEventListener("click", function (e) {
-    e.preventDefault()
+    e.preventDefault();
     app.quizSection.scrollIntoView({
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 };
